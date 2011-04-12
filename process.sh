4f654e441cd8be6868d724cd77c2dc81ff3a9942
Jim Klassen <klassen.js@gmail.com>
2011-04-12 15:05:13 -0500

    Optimize process.sh for multi-core machines.

--- process.sh
+++ process.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-#### Settings
+##################
+#### Settings ####
+##################
+
+### Maximum number of processes to run at once (=number of image strips processed at once)
+MAX_PAN=10  # Set to use all CPU cores, but don't cause swapping
+MAX_HSI=8   # Set to use all CPU cores, but don't cause swapping
+
+### Output resolution of GeoTIFFs, higher needs more memory per process.
 PAN_RES=.3048  # Pan-chromatic output resolution (meters/px)
 HSI_RES=1.0    # Hyper-spectral output resoluton (meters/px)
 
-AVERAGE_HEIGHT=259.0 # meters above mean sea level (WGS84)
-OUTPUT_PROJECTION="+init=epsg:26914" # Output projection (proj4 syntax)
-Archer2GeoTIFF="/home/archer/Archer/Archer2GeoTIFF/Archer2GeoTIFF"
-Archer2GeoTIFF="/Users/jimk/Archer/Archer2GeoTIFF/build/release/Archer2GeoTIFF"
+### Projection Settings
+# Grand Forks
+#AVERAGE_HEIGHT=259.0 # meters above mean sea level (WGS84) 
+#OUTPUT_PROJECTION="+init=epsg:26914" # Output projection (proj4 syntax)
+
+# St. Paul
+AVERAGE_HEIGHT=213.0 # meters above mean sea level (WGS84)
+OUTPUT_PROJECTION="+init=epsg:26915" # Output projection (proj4 syntax)
+
+### Path to Archer2GeoTIFF binary
 Archer2GeoTIFF="Archer2GeoTIFF"
+
+################################################################
+#### Main Script - Shouldn't need to change below this line ####
+################################################################
 
 SOURCE=$1
 DEST=$2
 
+RUNNING=0
+
 # Process Archer -> GeoTIFF
 for i in "$SOURCE"/*pan ; do 
-	$Archer2GeoTIFF -r $PAN_RES -e $AVERAGE_HEIGHT -p $OUTPUT_PROJECTION -i $i -o "$DEST"/`basename $i.tif`
+	$Archer2GeoTIFF -r $PAN_RES -e $AVERAGE_HEIGHT -p $OUTPUT_PROJECTION -i $i -o "$DEST"/`basename $i.tif` &
+	RUNNING=`expr 1 + $RUNNING`
+	if([ $RUNNING -ge $MAX_PAN ]); then
+		wait
+		RUNNING=0
+	fi
 done
 for i in "$SOURCE"/*hsi ; do 
-	$Archer2GeoTIFF -r $HSI_RES -e $AVERAGE_HEIGHT -p $OUTPUT_PROJECTION -i $i -o "$DEST"/`basename $i.tif`
-done
-
-# Create compressed GeoTIFFs
-mkdir -p "$DEST/compressed"
-for i in "$DEST"/*pan.tif ; do 
-	gdal_translate -of GTiff -co COMPRESS=JPEG -co TILED=YES $i "$DEST"/compressed/`basename $i`
-done
-for i in "$DEST"/*hsi.tif ; do 
-	gdal_translate -of GTiff -co COMPRESS=LZW  $i $DEST/compressed/`basename $i`
-done
-
-
+	$Archer2GeoTIFF -r $HSI_RES -e $AVERAGE_HEIGHT -p $OUTPUT_PROJECTION -i $i -o "$DEST"/`basename $i.tif` &
+	RUNNING=`expr 1 + $RUNNING`
+	if([ $RUNNING -ge $MAX_HSI ]); then
+		wait
+		RUNNING=0
+	fi
+done 
+wait
