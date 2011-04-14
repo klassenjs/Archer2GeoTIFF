bfcabb362153bf9873a89d109798b804b663b3ab
Jim Klassen <klassen.js@gmail.com>
2011-04-14 13:31:42 -0500

    Update build script to automatically choose Mac vs. Linux (Debian/Ubuntu) build.

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-## Build script for Linux
 
-#g++ main.cpp ArcherDataset.cpp ArcherRectify.cpp -lgdal1.5.0 -I/usr/include/gdal -O3 -o Archer2GeoTIFF
+TYPE=`uname -s`
 
-## Build script for Mac
+if [ "${TYPE}" = "Darwin" ]; then
 
-g++ main.cpp ArcherDataset.cpp ArcherRectify.cpp -lgdal -lproj -I/usr/local/osgeo/include -L/usr/local/osgeo/lib -O3 -o Archer2GeoTIFF
+	## Build script for Mac
+	g++ main.cpp ArcherDataset.cpp ArcherRectify.cpp \
+		-lgdal -lproj \
+		-I/usr/local/osgeo/include \
+		-L/usr/local/osgeo/lib \
+		-O3 -o Archer2GeoTIFF
+else
+	## Build script for Linux
+	g++ main.cpp ArcherDataset.cpp ArcherRectify.cpp \
+		-lgdal1.6.0 \
+		-lproj \
+		-I/usr/include/gdal \
+		-O3 -o Archer2GeoTIFF
+	#g++ main.cpp ArcherDataset.cpp ArcherRectify.cpp -Wl,-R/home/jimk/apps/lib -L/home/jimk/apps/lib -lgdal -lproj -lopenjpeg -I/home/jimk/apps/include -O3 -o Archer2GeoTIFF
+
+fi
 
